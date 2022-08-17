c9b9b350207de4e60865b07b0fa43a93502485fc
zhenyuWang <13641039885@163.com>
2022-08-17 14:23:22 +0800

    feat(api): createInnerAudioContext

---
+++ api/createInnerAudioContext/pages/index/index.vue
@@ -0,0 +1,87 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<button type="primary" @click="onPlay">on play</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				innerAudioContext: null
+			}
+		},
+		onLoad() {
+
+		},
+		onReady(){
+			// this.innerAudioContext = uni.createInnerAudioContext();
+			// // this.innerAudioContext.autoplay = true;
+			// this.innerAudioContext.play()
+			// this.innerAudioContext.src = 'https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-hello-uniapp/2cc220e0-c27a-11ea-9dfb-6da8e309e0d8.mp3';
+			// this.innerAudioContext.onPlay(() => {
+			//   console.log('开始播放');
+			// });
+			// this.innerAudioContext.onError((res) => {
+			//   console.log(res.errMsg);
+			//   console.log(res.errCode);
+			// });
+		},
+		methods: {
+			onPlay(){
+				this.innerAudioContext = uni.createInnerAudioContext();
+				// this.innerAudioContext.autoplay = true;
+				this.innerAudioContext.src = 'https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-hello-uniapp/2cc220e0-c27a-11ea-9dfb-6da8e309e0d8.mp3';
+				this.innerAudioContext.play()
+				this.innerAudioContext.onPlay(() => {
+				  console.log('开始播放');
+				});
+				this.innerAudioContext.onError((res) => {
+				  console.log(res.errMsg);
+				  console.log(res.errCode);
+				});
+				this.innerAudioContext.onTimeUpdate((res) => {
+					console.log(this.innerAudioContext.currentTime)
+					console.log(this.innerAudioContext.duration)
+				});
+				this.innerAudioContext.onEnded((res) => {
+					console.log('on end');
+				});
+				// this.innerAudioContext.play()
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ api/createVideoContext/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="content">
+		<video id="myVideo"
+			src="https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20200317.mp4"
+			controls></video>
+			<button type="primary" @click="play">play</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				videoContext: null
+			}
+		},
+		onLoad() {
+
+		},
+		onReady() {
+			console.log('globalData',getApp().globalData.text)
+		},
+		methods: {
+			play(){
+				console.log('play');
+				this.videoContext = uni.createVideoContext('myVideo', this);
+				// this.videoContext = uni.createVideoContext('myVideo');
+				this.videoContext.play();
+			}
+		}
+		
+	}
+</script>
