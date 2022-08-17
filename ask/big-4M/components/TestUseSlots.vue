967de7c4af1b01baad4f1f8533d76baf54983e19
zhenyuWang <13641039885@163.com>
2022-08-17 14:30:31 +0800

    feat: big-4M

---
+++ ask/big-4M/components/TestUseSlots.vue
@@ -0,0 +1,16 @@
+<template>
+	<view class="">
+		<view>test useSlots</view>
+		<slot></slot>
+	</view>
+</template>
+
+<script setup>
+	import {useSlots, onMounted} from 'vue'
+	
+	onMounted(() => {
+		const slots = useSlots()
+		console.log('slots', slots)
+		console.log('slots.default',slots.default);
+	})
+</script>
---
+++ ask/big-4M/pages/index/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<image class="test" src="/static/test.jpg"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<TestUseSlots>
+			<view class="">test useSlots default slot</view>
+		</TestUseSlots>
+	</view>
+</template>
+
+<script setup>
+	import TestUseSlots from '@/components/TestUseSlots.vue'
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
