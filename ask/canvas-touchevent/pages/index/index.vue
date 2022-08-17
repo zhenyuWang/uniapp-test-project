bdf5d541275a86e5d2fac47fc956fc2696716aae
zhenyuWang <13641039885@163.com>
2022-08-17 14:31:19 +0800

    feat: canvas touch event

---
+++ ask/canvas-touchevent/pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="content">
+		<canvas canvas-id="canvas" class="canvas" @touchstart.stop="handleTouchstart" @touchmove.stop="handleTouchmove" @touchend.stop="handleTouchend"></canvas>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			handleTouchstart(e){
+				console.log('handleTouchstart',e);
+			},
+			handleTouchmove(e){
+				console.log('handleTouchmove',e);
+			},
+			handleTouchend(e){
+				console.log('handleTouchend',e);
+			}
+		}
+	}
+</script>
+
+<style>
+	.canvas {
+		width: 610rpx;
+		height: 610rpx;
+		background-color: #fff;
+		border: 1px solid red;
+	}
+</style>
