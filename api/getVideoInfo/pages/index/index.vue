9ebca3b56f51b5824fd558eec2c6507526b00494
zhenyuWang <13641039885@163.com>
2022-08-17 14:26:10 +0800

    feat(api): getVideoInfo

---
+++ api/getVideoInfo/pages/index/index.vue
@@ -0,0 +1,68 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<button @click="getVideoInfo">getVideoInfo</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			getVideoInfo() {
+				uni.chooseVideo({  
+					sourceType: ['album'],  
+					compressed: false,  
+					success:(res) => {  
+						console.log('chooseVideo',res);  
+						uni.getVideoInfo({  
+							filePath: res.tempFilePath,  
+							success: (res2) => {  
+								console.log('getVideoInfo',res2);  
+							}  
+						});  
+					}  
+				})  
+			}
+		}
+	}
+</script>
+
+<style>
+	
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
