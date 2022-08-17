bc7a43495308da5d5ae33664161c40448f2d7bdf
zhenyuWang <13641039885@163.com>
2022-08-17 10:48:00 +0800

    feat: add pages-jump

---
+++ normal/component/pages-jump/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="box">
+		<view>this is index</view>
+		<navigator class="navigator" url="/pages/about/index">go about</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.navigator{
+		padding:9px 10px;
+		background-color: #09c;
+		text-align: center;
+		color: #fff;
+	}
+</style>
