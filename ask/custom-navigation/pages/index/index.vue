0f99047b2b18d787ffb01803651ac3d603481d3c
zhenyuWang <13641039885@163.com>
2022-08-18 14:42:37 +0800

    feat: custom navigation

---
+++ ask/custom-navigation/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+		<button hover-class='button-hover' class="test-button">goTest</button>
+		<button hover-class='button-hover' type="primary">goTest</button>
+		<button hover-class='button-hover' type="warn">goTest</button>
+		<Popup></Popup>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+			}
+		},
+	}
+</script>
+
+<style>
+	.test-button{
+		background-color: aqua;
+	}
+	.test-button[disabled]{
+		background-color: red!important;
+	}
+	.button-hover {
+		background-color: red;
+	}
+</style>
+
