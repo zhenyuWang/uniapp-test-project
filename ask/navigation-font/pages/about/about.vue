4aa6ce17e9a290db565d37d0ddcafa1ea262e264
zhenyuWang <13641039885@163.com>
2022-08-18 14:48:34 +0800

    feat: navigation custom font

---
+++ ask/navigation-font/pages/about/about.vue
@@ -0,0 +1,46 @@
+<template>
+	<view>
+		<button @click="back">back</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		created(){
+			console.log('about created');
+		},
+		onBackPress(event) {
+			console.log("e", event);  
+			//e, [Object] {"from":"backbutton"}  
+			if (event.from == 'backbutton') {  
+				uni.showModal({  
+					title: '提示',  
+					content: '退出后已填写的数据会丢失,是否继续退出?',  
+					showCancel: true,  
+					confirmText: '退出',  
+					success: (res) => {  
+						if (res.confirm) {  
+							uni.navigateBack()  
+						}  
+					}  
+				})  
+				return true;  
+			}  
+		},
+		methods: {
+			back(){
+				uni.navigateBack()
+				
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
