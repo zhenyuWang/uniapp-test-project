081a1dd499624755a553978d5d1ade4ab36ba2b7
zhenyuWang <13641039885@163.com>
2022-08-18 14:49:59 +0800

    feat: url params

---
+++ ask/url-params/pages/test/test.vue
@@ -0,0 +1,17 @@
+<template>
+	<view>
+		this is test page
+	</view>
+</template>
+
+<script setup>
+import {onLoad} from '@dcloudio/uni-app';
+
+onLoad((params) => {
+	console.log('onLoad',params)
+})
+</script>
+
+<style>
+
+</style>
