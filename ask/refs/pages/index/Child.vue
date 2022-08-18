fd7f96b45db69c3043a96d247ec1daa3032b5bb3
zhenyuWang <13641039885@163.com>
2022-08-18 14:49:00 +0800

    feat: refs

---
+++ ask/refs/pages/index/Child.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="">
+		this is child
+	</view>
+</template>
+
+<script>
+	export default {
+		methods:{
+			test(){
+				console.log('child test');
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ ask/refs/pages/index/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="content">
+		<child ref='child'></child>
+		<button type="primary" @click="getRefChild">getRefChild</button>
+		<view v-for="(item, index) in list" :key="index" :open="true">  
+			<child :ref="getRef(index)" />  
+		</view>
+	</view>
+</template>
+
+<script>
+	import Child from './Child.vue'
+	export default {
+		components:{Child},
+		data() {
+			return {
+				list:[0,1,2,3,4]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			getRefChild(){
+				// console.log('this.$refs',this.$refs)
+				// console.log('this.$refs.child',this.$refs.child)
+				// console.log('this.$refs.child0',this.$refs.child0)
+				// console.log('this.$refs.child1',this.$refs.child1)
+				// console.log('this.$refs.child2',this.$refs.child2)
+				// console.log('this.$refs.child3',this.$refs.child3)
+				this.$refs.child.test()
+				this.$refs.child0[0].test()
+				console.log(333);
+			},
+			getRef(index){
+				return `child${index}`
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
