0ad574c9ebe7538f314d7751d150dcb24716cbce
zhenyuWang <13641039885@163.com>
2022-08-18 14:49:25 +0800

    feat: slot

---
+++ ask/slot/pages/index/MySlot.vue
@@ -0,0 +1,26 @@
+
+<template>
+	<view>
+			<!-- <slot :name="'title'+index" :content="{name:'标题1'}"></slot> -->
+			<!-- <slot name="title1" :content="{name:'标题1'}"></slot> -->
+			<!-- <slot :name="'title'+index"></slot> -->
+		<view v-for="(item,index) in 3" :key="index">
+			<!-- <slot :name="'title'+index"></slot> -->
+			<!-- <slot name="title1" :content="{name:'标题1'}"></slot> -->
+			<slot :name="'title'+index" :content="{name:'标题'+index}"></slot>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				index:1
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ ask/slot/pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="content">
+		<MySlot>
+<!-- 			<template #title1="props">
+				111111 {{props}}
+			</template> -->
+		<!-- 	<template v-slot:title1>
+				111111
+			</template> -->
+			
+<!-- 			<template v-slot:title0>
+				000000
+			</template>
+			<template v-slot:title1>
+				111111
+			</template>
+			<template v-slot:title2>
+				222222
+			</template>
+			 -->
+			<template #title0="props1">
+				{{props1}} 111
+			</template>
+			<template #title1="props2">
+				{{props2}} 222
+			</template>
+			<template #title2="props3">
+				{{props3}} 333
+			</template>
+			
+		</MySlot>
+	</view>
+</template>
+
+<script>
+	import MySlot from './MySlot.vue'
+	export default {
+		components: {
+			MySlot
+		},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
