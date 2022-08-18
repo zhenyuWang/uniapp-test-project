e3521eb156247fe434488ba098c95b8c6ab8dc61
zhenyuWang <13641039885@163.com>
2022-08-18 14:43:00 +0800

    feat(toutiao): uploadDouyinVideo

---
+++ ask/douyin-uploadDouyinVideo/pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<button open-type="uploadDouyinVideo" id="1" data-hello="world">uploadDouyinVideo</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+		},
+		onUploadDouyinVideo(uploadOptions) {
+			console.log('onUploadDouyinVideoOptions hook: ', uploadOptions)
+		},
+		methods: {
+			textClick(){
+				console.log('text click');
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
