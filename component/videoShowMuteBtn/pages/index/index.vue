53f86a0b841c01cb9686bff4ff29eb17c87d5b7c
zhenyuWang <13641039885@163.com>
2022-08-19 08:53:19 +0800

    feat: video showMuteBtn

---
+++ component/videoShowMuteBtn/pages/index/index.vue
@@ -0,0 +1,7 @@
+<template>
+	<view class="uni-padding-wrap uni-common-mt">
+		<video id="myVideo"
+			src="https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20200317.mp4"
+			enable-danmu danmu-btn controls></video>
+	</view>
+</template>
