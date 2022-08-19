88a9d97423a8d30222d8241f6f6f2c5487cfdabc
zhenyuWang <13641039885@163.com>
2022-08-19 08:53:50 +0800

    feat: webview style

---
+++ component/webview-style/pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="content">
+		<button @click="changeUrl" type="primary">changeUrl</button>
+		<button @click="changeStyle" type="primary">changeStyle</button>
+		<web-view :style="testStyle" :fullscreen='false' :src="url"></web-view>
+		<!-- <web-view :style="testStyle" :fullscreen='true' :src="url"></web-view> -->
+		<!-- <web-view :fullscreen='false' :src="url"></web-view> -->
+		<!-- <web-view :fullscreen='true' :src="url"></web-view> -->
+		<!-- <web-view :src="url"></web-view> -->
+		<text>1231</text>
+		<navigator url="/pages/test/test">go test</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				url: 'http://114.115.235.59/test-webview1.html',
+				testStyle:{
+					width:'100px',
+					height:'100px'
+				}
+			}
+		},
+		methods:{
+			changeUrl(){
+				this.url = 'http://114.115.235.59/test-webview2.html'
+			},
+			changeStyle(){
+				this.testStyle = {
+					width:'300px',
+					height:'300px'
+				}
+			}
+		}
+	}
+</script>
---
+++ component/webview-style/pages/test/test.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<view>this is test page</view>
+		<navigator url="/pages/index/index">go index</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
