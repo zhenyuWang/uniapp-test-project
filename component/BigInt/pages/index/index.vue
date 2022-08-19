491c992cc58b7181d71813ccc71c57b07e8c3db3
zhenyuWang <13641039885@163.com>
2022-08-19 08:49:19 +0800

    feat: BigInt

---
+++ component/BigInt/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onReady() {
+			console.log('vue2')
+			const num = BigInt(100)
+			console.log('BigInt num',num)
+			const ws = new WeakSet()
+			const foo = {}
+			ws.add(foo)
+			console.log('weakSet',ws);
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
