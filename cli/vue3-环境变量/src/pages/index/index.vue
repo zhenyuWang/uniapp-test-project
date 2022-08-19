f9eef6145eedae712bc01b6ab9d80b6ed082a23f
zhenyuWang <13641039885@163.com>
2022-08-19 08:48:25 +0800

    feat: cli-vue3-环境变量

---
+++ cli/vue3-环境变量/src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="content">
+    <view>TEST: {{test}}</view>
+    <view>VITE_SOME_KEY: {{someKey}}</view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello',
+      test: process.env.TEST,
+      someKey: import.meta.env.VITE_SOME_KEY
+    }
+  },
+  onLoad() {},
+  methods: {},
+}
+</script>
