08a062f51fd92bcddbce9c581117ed58816b47e3
zhenyuWang <13641039885@163.com>
2022-09-02 11:39:12 +0800

    update: component map

---
+++ component/map/pages/getLocation/getLocation.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		getLocation
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onReady() {
+			uni.getLocation({
+				// type:'wgs84',
+				type: 'gcj02',
+				success:(res) => {
+					console.log('getLocation success',res);
+				},
+				complete(res) {
+					console.log('getLocation complete',res)
+				}
+			})
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- component/map/pages/openLocation/openLocation.vue
+++ component/map/pages/openLocation/openLocation.vue
@@ -16,24 +16,17 @@
 		},
 		methods: {
 			openLocation(){
-				uni.getLocation({
-					type: 'gcj02', //返回可以用于uni.openLocation的经纬度
+				uni.openLocation({
+					latitude: 39.909,
+					longitude: 116.39742,
+					scale:18,
+					name:'当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置',
+					address: '111详细地址详细地址详细地址详细地址详细地址详细地址详细地址详细地址详细地址详细地址详细地址详细地址',
 					success: function (res) {
-						const latitude = res.latitude;
-						const longitude = res.longitude;
-						uni.openLocation({
-							latitude,
-							longitude,
-							scale:18,
-							name:'当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置当前位置',
-							address: '111详细地址详细地址详细地址详细地址详细地址详细地址详细地址详细地址详细地址详细地址详细地址详细地址',
-							success: function (res) {
-								console.log('success',res);
-							},
-							fail: function(err){
-								console.log('err',err);
-							}
-						});
+						console.log('success',res);
+					},
+					fail: function(err){
+						console.log('err',err);
 					}
 				});
 			}
