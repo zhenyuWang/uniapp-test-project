10fb2ad87836a6eb682c7be7445695cfd58b1938
zhenyuWang <13641039885@163.com>
2022-09-17 23:08:05 +0800

    feat(cli): vite-vue3-ts

---
+++ cli/vite-vue3-ts/src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import { getCurrentInstance } from 'vue'
+console.log('App log vue',getCurrentInstance());
+
+</script>
+
+<template>
+  <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div> -->
+  <HelloWorld class="bgR" msg="Vite + Vue" />
+  <div>this is vite-vue3-ts</div>
+</template>
+
+<style>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.bgR{
+  height: 300px;
+  background-color: red;
+}
+</style>
---
+++ cli/vite-vue3-ts/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Foo from './Foo.vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <Foo class="bgB"></Foo>
+  </div>
+</template>
+
+<style>
+.read-the-docs {
+  color: #888;
+}
+.bgB{
+  height: 100px;
+  background-color: blue;
+}
+</style>
