cfc1232d56711603d112cec05c8c9c27ce79a917
zhenyuWang <13641039885@163.com>
2022-09-17 23:09:14 +0800

    update(cli): vue3

--- cli/vue3/src/pages/index/index.vue
+++ cli/vue3/src/pages/index/index.vue
@@ -4,51 +4,13 @@
     <view class="text-area">
       <text class="title">{{ title }}</text>
     </view>
-    <button type="primary" @click="onPlay">on play123</button>
   </view>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      title: 'Hello',
-      innerAudioContext: null
-    }
-  },
-  onLoad() {},
-  onReady(){
-    console.log('page index onReady');
-    // #ifdef APP-PLUS || MP-WEIXIN
-    console.log('小程序 or App')
-    // #endif
-    this.innerAudioContext = uni.createInnerAudioContext({
-      src: 'https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-hello-uniapp/2cc220e0-c27a-11ea-9dfb-6da8e309e0d8.mp3'
-    });
-    console.log('this.innerAudioContext',this.innerAudioContext);
-  },
-  methods: {
-    onPlay(){
-      console.log('on play')
-      console.log('this.innerAudioContext',this.innerAudioContext.src);
-      // this.innerAudioContext.play()
-      // this.innerAudioContext.onPlay(() => {
-      //   console.log('开始播放');
-      // });
-      // this.innerAudioContext.onError((res) => {
-      //   console.log(res.errMsg);
-      //   console.log(res.errCode);
-      // });
-      // this.innerAudioContext.onTimeUpdate((res) => {
-      //   console.log(this.innerAudioContext.currentTime)
-      //   console.log(this.innerAudioContext.duration)
-      // });
-      // this.innerAudioContext.onEnded((res) => {
-      //   console.log('on end');
-      // });
-    }
-  },
-}
+<script setup>
+import {ref} from 'vue'
+const title = ref('hello world')
+console.log('title',title)
 </script>
 
 <style>
