49ded9f6c6d8c61fc422d972dd7cee088ddbdc6d
zhenyuWang <13641039885@163.com>
2022-09-17 23:23:19 +0800

    feat(ask): axios

---
+++ ask/axios/main.js
@@ -0,0 +1,28 @@
+import App from './App'
+
+// #ifndef VUE3
+import Vue from 'vue'
+import store from './store'
+Vue.config.productionTip = false
+Vue.prototype.$store = store
+
+App.mpType = 'app'
+const app = new Vue({
+	store,
+    ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+import store from './store'
+console.log('vue3 store',store);
+export function createApp() {
+  const app = createSSRApp(App)
+	app.use(store)
+  return {
+    app
+  }
+}
+// #endif
---
+++ ask/axios/store/index.js
@@ -0,0 +1,24 @@
+// 页面路径：store/index.js 
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+
+// Vue.use(Vuex);//vue的插件机制
+
+// //Vuex.Store 构造器选项
+// const store = new Vuex.Store({
+// 	state:{//存放状态
+// 		"username":"foo",
+// 		"age":18
+// 	}
+// })
+// export default store
+
+import { createStore } from 'vuex'
+const store = createStore({
+	state:{//存放状态
+		"username":"foo",
+		"age":18
+	}
+})
+
+export default store
