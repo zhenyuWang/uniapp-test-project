f7ff633b1eca93bd97040cf618041537f81f4252
zhenyuWang <13641039885@163.com>
2022-10-13 09:53:11 +0800

    feat(api): chooseImage

---
+++ api/chooseImage/pages/index/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<button type="primary" @click="chooseImage">chooseImage</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			chooseImage(){
+				uni.chooseImage({
+					count:1,
+					// sizeType:['compressed'],
+					success(res){
+						console.log('res',res)
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
