e44b072732d365dc637a0e17cc3a1e9c265a8f01
zhenyuWang <13641039885@163.com>
2022-10-13 10:11:11 +0800

    feat(mp-kuaishou): registerMetaData

---
+++ mp/kuaishou/registerMetaData/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="content">
+		<button type="primary" @click="test">test</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		registerMetaData(options){
+			console.log('registerMetaData-----------------',options)
+		},
+		methods: {
+			test(){
+				this.$scope.registerMetaData('test registerMetaData')
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
