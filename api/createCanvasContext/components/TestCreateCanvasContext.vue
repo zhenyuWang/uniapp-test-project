98d2459c1955814afcb6dce29e59b6fe34497d17
zhenyuWang <13641039885@163.com>
2022-10-13 09:53:33 +0800

    feat(api): createCanvasContext

---
+++ api/createCanvasContext/components/TestCreateCanvasContext.vue
@@ -0,0 +1,62 @@
+<template>
+	<view>
+		<canvas canvas-id="TestCreateCanvasContext" style="border:1px solid #09c;"></canvas>
+		<video id="myVideo" src="https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-uni-app-doc/360e4b20-4f4b-11eb-8a36-ebb87efcf8c0.mp4"></video>
+		<map id="map" class="map" :show-location="true"></map>
+		<view class="scroll"></view>
+		<view class="test"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"TestCreateCanvasContext",
+		data() {
+			return {
+				
+			};
+		},
+		onReady(){
+			const ctx = uni.createCanvasContext('TestCreateCanvasContext',this)
+			console.log('TestCreateCanvasContext ctx',ctx)
+			
+			ctx.fillRect(0,0,10,10)
+			ctx.draw()
+			
+			uni.canvasToTempFilePath({
+				canvasId:'TestCreateCanvasContext',
+				x:0,
+				y:0,
+				width:120,
+				height:120,
+				success(res){
+					console.log('canvasToTempFilePath',res);
+				}
+			},this)
+			uni.canvasGetImageData({
+				canvasId:'TestCreateCanvasContext',
+				x:0,
+				y:0,
+				width:10,
+				height:10,
+				success(res){
+					console.log('canvasGetImageData',res)
+				}
+			},this)
+			
+			const videoContext = uni.createVideoContext('myVideo',this)
+			console.log('videoContext',videoContext);
+			
+			const mapContext = uni.createMapContext("map", this);
+			console.log('mapContext',mapContext);
+			
+			const mediaQueryOb = uni.createMediaQueryObserver(this)
+			console.log('mediaQueryOb',mediaQueryOb);
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ api/createCanvasContext/pages/index/index.vue
@@ -0,0 +1,80 @@
+<template>
+	<view @click="click" class="content">
+		<TestCreateCanvasContext />
+		<canvas canvas-id="index" style="border:1px solid #ccc;"></canvas>
+	</view>
+</template>
+
+<script>
+	import TestCreateCanvasContext from '../../components/TestCreateCanvasContext.vue'
+	export default {
+		components:{TestCreateCanvasContext},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onReady() {
+			
+		},
+		methods: {
+			click(){
+				console.log('this',this)
+				const ctx = uni.createCanvasContext('index',this)
+				console.log('ctx123',ctx)
+				uni.canvasToTempFilePath({
+					canvasId:'index',
+					x:0,
+					y:0,
+					width:120,
+					height:120,
+					success(res){
+						console.log('canvasToTempFilePath',res);
+					}
+				},this)
+				
+				const data = new Uint8ClampedArray([255, 0, 0, 255,255, 0, 0, 255,255, 0, 0, 255,255, 0, 0, 255,255, 0, 0, 255])
+				uni.canvasPutImageData({
+				  canvasId: 'index',
+				  x: 10,
+				  y: 10,
+				  width: 2,
+					height: 2,
+				  data: data,
+				  complete(res) {
+						console.log('canvasPutImageData complete',res);
+					}
+				})
+
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
