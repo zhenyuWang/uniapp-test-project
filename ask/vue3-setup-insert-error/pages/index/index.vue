a27c86ff9a9304831fdbf18d113e4235aaf34ffa
zhenyuWang <13641039885@163.com>
2022-10-13 10:09:50 +0800

    feat(mp-weixin): arr reverse insert error

---
+++ ask/vue3-setup-insert-error/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>  
+  <view>  
+    <button @click="reverse">reverse</button>  
+    <view v-for="(item,index) in list" :key="item">{{ item }}</view>  
+  </view>  
+</template>  
+
+<script>
+export default {
+	data(){
+		return {
+			list: ["a", "b", "c", "d"]
+		}
+	},
+	methods:{
+		reverse() {
+		  this.list.reverse()
+			console.warn(`reversed list:${JSON.stringify(this.list)}`)
+		}
+	}
+}
+</script>
