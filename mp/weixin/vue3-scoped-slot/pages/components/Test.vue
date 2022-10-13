5e4ac19a1d52389da17ddeb79fd782c83cb4785e
zhenyuWang <13641039885@163.com>
2022-10-13 10:07:06 +0800

    feat(mp-weixin): scoped slot

---
+++ mp/weixin/vue3-scoped-slot/pages/components/Test.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<view v-for="item in list" :key="item.id">
+			<view>测试作用域插槽</view>
+			<slot :name="item.slotName" :model="item.model"></slot>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				list:[
+					{
+						slotName:'test1',
+						model:'model1'
+					},{
+						slotName:'test2',
+						model:'model2'
+					},{
+						slotName:'test3',
+						model:'model3'
+					}
+				]
+			}
+		}
+	}
+</script>
---
+++ mp/weixin/vue3-scoped-slot/pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+		<Test>
+			<template v-for="item in list">
+				<template #[item.slotName]="{model}">
+					vue3 作用域插槽未渲染:{{model}}
+				</template>
+			</template>
+		</Test>
+</template>
+
+<script>
+	import Test from '../components/Test.vue'
+	export default {
+		components:{Test},
+		data(){
+			return {
+				list:[
+					{
+						slotName:'test1',
+						model:'model1'
+					},{
+						slotName:'test2',
+						model:'model2'
+					},{
+						slotName:'test3',
+						model:'model3'
+					}
+				]
+			}
+		}
+	}
+</script>
