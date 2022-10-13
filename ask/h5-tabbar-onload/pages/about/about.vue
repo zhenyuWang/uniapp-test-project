a5a296486c69f73112e6295b34a4364eefc8619d
zhenyuWang <13641039885@163.com>
2022-10-13 09:54:52 +0800

    feat(ask): h5 tabbar onload

---
+++ ask/h5-tabbar-onload/pages/about/about.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<navigator url="/pages/setting/setting">go setting</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad(){
+			console.log('about onLoad');
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ ask/h5-tabbar-onload/pages/setting/setting.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<button @click="goIndex">goIndex</button>
+		<button @click="goAbout">goAbout</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad(){
+			console.log('setting onLoad');
+		},
+		methods: {
+			goIndex(){
+				uni.switchTab({
+					url:'/pages/index/index'
+				})
+			},
+			goAbout(){
+				uni.switchTab({
+					url:'/pages/about/about'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
