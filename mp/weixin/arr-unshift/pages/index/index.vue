6c7d573e5483940bffd21354c94406d187ff7ad1
zhenyuWang <13641039885@163.com>
2022-10-13 10:09:02 +0800

    feat(mp-weixin): arr unshift

---
+++ mp/weixin/arr-unshift/pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="content">
+		<view v-for="item in list" :key="item.id">
+			<view class="item" @click="showId(item.id)">{{ item.id }}</view>
+		</view>
+		<button type="primary" @click="arrUnshift">unshift</button>
+		<view>curId:{{ curId }}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: [{
+					id: 1
+				}, {
+					id: 2
+				},{
+					id: 3
+				},{
+					id: 4
+				},{
+					id: 5
+				}],
+				curId: '',
+				nextId: 6
+			}
+		},
+		methods: {
+			showId(value) {
+				this.curId = value
+			},
+			arrUnshift() {
+				this.list.unshift({
+					id: this.nextId++
+				})
+			}
+		}
+	}
+</script>
