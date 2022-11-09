d6061d86ac8def244e9572792db33e5874a42b62
zhenyuWang <13641039885@163.com>
2022-11-09 14:40:49 +0800

    feat(api): onNavigationBarSearchInputConfirmed

---
+++ api/onNavigationBarSearchInputConfirmed/pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="content">
+		onNavigationBarSearchInputConfirmed
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		onNavigationBarSearchInputConfirmed(){
+			console.log('onNavigationBarSearchInputConfirmed');
+			alert("onNavigationBarSearchInputConfirmed")
+		},
+		methods: {
+
+		}
+	}
+</script>
+
