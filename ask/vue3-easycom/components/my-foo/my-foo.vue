80bf363432942fe2a2fcd1a73b29bdb658f9dc63
zhenyuWang <13641039885@163.com>
2022-11-09 14:49:56 +0800

    feat(ask): vue3 easycom

---
+++ ask/vue3-easycom/components/my-foo/my-foo.vue
@@ -0,0 +1,16 @@
+<template>
+	<view class="my-foo">
+    my-foo component text is:{{text}}
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			text:{
+				type:String,
+				default:''
+			}
+		}
+	}
+</script>
---
+++ ask/vue3-easycom/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="content">
+		<view>hx create project test vue3 easycom</view>
+		<my-foo :text="text" />
+		<uni-data-checkbox v-model="value" :localdata="range"></uni-data-checkbox>
+	</view>
+</template>
+
+<script setup lang="ts">
+	const text = 'test text'
+	const value = 0
+	const range = [{"value": 0,"text": "篮球"	},{"value": 1,"text": "足球"},{"value": 2,"text": "游泳"}]
+</script>
