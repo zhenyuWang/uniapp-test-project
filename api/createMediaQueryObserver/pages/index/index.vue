69c1f5923565659e035c238b173429554575794a
zhenyuWang <13641039885@163.com>
2022-11-09 14:34:11 +0800

    feat(api): createMediaQueryObserver

---
+++ api/createMediaQueryObserver/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="content">
+		<button @click="test" type="primary">test</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			test(){
+				const mediaQueryOb = uni.createMediaQueryObserver(this)
+				console.log('mediaQueryOb',mediaQueryOb);
+			}
+		}
+	}
+</script>
