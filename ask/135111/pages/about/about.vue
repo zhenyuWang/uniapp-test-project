5daa4d288f2f947d42a8ebac6d3db8385d4e15d5
zhenyuWang <13641039885@163.com>
2022-11-09 14:43:25 +0800

    feat(ask): 13511

---
+++ ask/135111/pages/about/about.vue
@@ -0,0 +1,49 @@
+<template>
+	<view>
+		<view class="block">
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad(){
+			console.log('this is about onLoad');
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.block {
+		width: 150px;
+		height: 900px;
+		background-color: aqua;
+	}
+</style>
---
+++ ask/135111/pages/index/index.vue
@@ -0,0 +1,79 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<navigator url="/pages/about/about">go about</navigator>
+		<view class="block">
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+			<view>11111</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			console.log('this is index onLoad');
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background-color: antiquewhite;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	
+	.block{
+		width: 150px;
+		height: 900px;
+		background-color: aqua;
+	}
+</style>
