8f6328e25115fc619f554117d900cf25625b8b63
zhenyuWang <13641039885@163.com>
2023-03-15 15:00:25 +0800

    feat(4175): subPackages

---
+++ issues/4175-subpackages/subPackageA/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		this is subPackageA index page
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
