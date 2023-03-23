fb9251b844256b745ab4c37a33b31b0295e7eff1
zhenyuWang <13641039885@163.com>
2023-03-23 21:18:48 +0800

    feat(api): swtichTab

---
+++ api/switchTab/pages/about/about.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		this is about
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onShow(){
+			console.log('page about onShow');
+		}
+	}
+</script>
+
+<style>
+
+</style>
