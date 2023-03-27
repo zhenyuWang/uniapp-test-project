0c2fab878cb64afd5e7e2c0ea039c9f2dd203ce6
zhenyuWang <13641039885@163.com>
2023-03-27 10:34:05 +0800

    feat(ask): 166132

---
+++ ask/166132-weixin-slot-component-class/components/Bar.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<view class="c-red">this is component Bar class is c-red</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"Bar",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+	.c-red{
+		color: red;
+	}
+</style>
---
+++ ask/166132-weixin-slot-component-class/components/Foo.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		<view>this is component Foo</view>
+		<slot></slot>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"Foo",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
