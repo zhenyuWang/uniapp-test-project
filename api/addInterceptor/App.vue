31c3d7dae641362437649856b29b88da7aecb0da
zhenyuWang <13641039885@163.com>
2023-04-12 20:03:49 +0800

    feat(api): addInterceptor

---
+++ api/addInterceptor/App.vue
@@ -0,0 +1,36 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+			// navigateTo switchTab
+			uni.addInterceptor('switchTab', {
+				invoke(args) {
+					console.warn('args.url', args.url)
+					uni.reLaunch({
+						url: '/pages/login/login'
+					})
+					return false
+				}
+			})
+			
+			uni.addInterceptor('request', {  
+			    success(res) {  
+			      console.log('addInterceptor request success',res)
+			    },  
+			    fail(err) {  
+			      console.log('addInterceptor request fail',err)
+			    }  
+			})
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ api/addInterceptor/pages/index/index.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			uni.request({
+				url:'http://localhost:8090',
+				success(res) {
+					console.log('index request success', res);
+				},
+				fail(res) {
+					console.log('index request fail', res);
+				}
+			})
+			uni.request({
+				url:'http://localhost:8090/err?code=500',
+				success(res) {
+					console.log('index request/error success', res);
+				},
+				fail(res) {
+					console.log('index request/error fail', res);
+				}
+			})
+			
+			fetch('http://localhost:8090/err?code=500').then(res => {
+				console.log('fetch success',res)
+			}).catch(err => {
+				console.log('fetch fail', err);
+			})
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
