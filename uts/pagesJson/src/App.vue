34a04ff05f314d533ab301fbf6278aa9bf88b5df
zhenyuWang <13641039885@163.com>
2023-05-18 23:20:35 +0800

    feat(uts): pagesJson

---
+++ uts/pagesJson/src/App.vue
@@ -0,0 +1,36 @@
+<script lang="uts">
+export default {
+  onLaunch: function () {
+    console.log("App Launch");
+  },
+  onShow: function () {
+    console.log("App Show");
+  },
+  onHide: function () {
+    console.log("App Hide");
+  },
+};
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.button{
+  margin-left: 20px;
+  margin-top: 20px;
+  width:140px;
+  height:40px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  background-color: #000;
+}
+.button-text{
+  font-size: 16px;
+  color: #fff;
+}
+</style>
