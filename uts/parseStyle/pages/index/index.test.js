6c49fb09f2f0804b9edf7e36787e2eda35efd54c
zhenyuWang <13641039885@163.com>
2023-06-08 20:19:49 +0800

    feat(test): update automator  project

---
+++ uts/parseStyle/pages/index/index.test.js
@@ -0,0 +1,16 @@
+describe('pages/index/index', () => {
+	let page
+	beforeAll(async () => {
+		page = await program.reLaunch('/pages/index/index')
+		await page.waitFor(1000)
+	})
+	it('css', async () => {
+		// const image = await program.screenshot({fullPage: true})
+		const image = await program.screenshot()
+		expect(image).toMatchImageSnapshot();
+	})
+	// it('methodWithCallback', () => {
+	// 	// page.callMethodWithCallback('getStorageInfo')
+	// 	page.callMethodWithCallback('getStorageInfo222')
+	// })
+})
