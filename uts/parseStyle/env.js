cb57b1736cfca197419774f6c606184e12de7826
zhenyuWang <13641039885@163.com>
2023-06-09 23:31:37 +0800

    feat: update test project

--- uts/parseStyle/env.js
+++ uts/parseStyle/env.js
@@ -1,1 +1,37 @@
-module.exports = {"compile":true,"h5":{"options":{"headless":true},"executablePath":""},"mp-weixin":{"port":9420,"account":"","args":"","cwd":"","launch":true,"teardown":"disconnect","remote":false,"executablePath":""},"app-plus":{"android":{"id":"bc75caa3","executablePath":"/Applications/HBuilderX-Alpha-6.8-2.app/Contents/HBuilderX/plugins/launcher/base/android_base.apk"},"version":"/Applications/HBuilderX-Alpha-6.8-2.app/Contents/HBuilderX/plugins/launcher/base/version.txt","ios":{"id":"","executablePath":""},"uni-app-x":{"version":"/Applications/HBuilderX-Alpha-6.8-2.app/Contents/HBuilderX/plugins/uniappx-launcher/base/version.txt","android":{"id":"bc75caa3","executablePath":"/Applications/HBuilderX-Alpha-6.8-2.app/Contents/HBuilderX/plugins/uniappx-launcher/base/android_base.apk"}}}}
+module.exports = {
+	"compile": true,
+	"h5": {
+		"options": {
+			"headless": true
+		},
+		"executablePath": ""
+	},
+	"mp-weixin": {
+		"port": 9420,
+		"account": "",
+		"args": "",
+		"cwd": "",
+		"launch": true,
+		"teardown": "disconnect",
+		"remote": false,
+		"executablePath": ""
+	},
+	"app-plus": {
+		"android": {
+			"id": "bc75caa3",
+			"executablePath": "/Applications/HBuilderX-Alpha-6.8-2.app/Contents/HBuilderX/plugins/launcher/base/android_base.apk"
+		},
+		"version": "/Applications/HBuilderX-Alpha-6.8-2.app/Contents/HBuilderX/plugins/launcher/base/version.txt",
+		"ios": {
+			"id": "",
+			"executablePath": ""
+		},
+		"uni-app-x": {
+			"version": "/Applications/HBuilderX-Alpha-6.8-2.app/Contents/HBuilderX/plugins/uniappx-launcher/base/version.txt",
+			"android": {
+				"id": "bc75caa3",
+				"executablePath": "/Applications/HBuilderX-Alpha-6.8-2.app/Contents/HBuilderX/plugins/uniappx-launcher/base/android_base.apk"
+			}
+		}
+	}
+}
