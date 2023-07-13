934e290e494ccd5f84149682aabcceb30831779a
zhenyuWang <13641039885@163.com>
2023-07-13 22:02:38 +0800

    feat(uts): simple automator

---
+++ uts/uts-simple-automator/pages/index/index.test.js
@@ -0,0 +1,11 @@
+describe('pages/index/index', () => {
+	let page
+	beforeAll(async () => {
+		page = await program.reLaunch('/pages/index/index')
+		console.log('page-------------',page)
+		await page.waitFor(1000)
+	})
+	it('test', async () => {
+		expect(2).toEqual(2)
+	})
+})
