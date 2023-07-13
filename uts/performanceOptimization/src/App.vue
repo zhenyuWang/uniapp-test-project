4b390849e613920a03a22145b2038d64fafc35bc
zhenyuWang <13641039885@163.com>
2023-07-13 22:01:14 +0800

    feat(uts): performance optimization

---
+++ uts/performanceOptimization/src/App.vue
@@ -0,0 +1,22 @@
+<script lang="uts">
+export default {
+  onLaunch: function () {
+    println("App Launch");
+  },
+  onShow: function () {
+    println("App Show");
+  },
+  onHide: function () {
+    println("App Hide");
+  },
+};
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
