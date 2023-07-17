07618167efef94da4ef10709481620e150d7b5f0
zhenyuWang <13641039885@163.com>
2023-07-17 20:57:58 +0800

    feat: pageScroll & onReachBottom

---
+++ api/pageScroll-onReachBottom/pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <view class="uni-padding-wrap container">
+		<button @click="pageScrollTo">scrollTo 1200</button>
+  </view>
+</template>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      data: '',
+    }
+  },
+  onReachBottom() {
+    console.log('onReachBottom')
+  },
+
+  methods: {
+
+		pageScrollTo(){
+			uni.pageScrollTo({
+				scrollTop: 1200,
+				complete(res){
+					console.warn('pageScrollTo', res)
+				}
+			})
+		}
+  },
+}
+</script>
+
+<style>
+.container {
+  height: 1200px;
+}
+</style>
