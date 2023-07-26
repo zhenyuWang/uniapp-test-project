3bd12ca31cfd5ef909a805b1842336c18a30beb3
zhenyuWang <13641039885@163.com>
2023-07-26 09:53:29 +0800

    feat(uts): templete

---
+++ uts/uts-templete/App.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+export default {
+  onLaunch: function () {
+    console.log('App Launch');
+  },
+  onShow: function () {
+    console.log('App Show');
+  },
+  onHide: function () {
+    console.log('App Hide');
+  },
+  onLastPageBackPress: function (): boolean | null {
+    uni.showToast({
+      title: '再按一次退出应用',
+      position: 'bottom',
+    });
+    return null;
+  },
+};
+</script>
+
+<style>
+/*每个页面公共css */
+</style>
