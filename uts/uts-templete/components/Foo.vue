14f83e2859883ef0c23d3cae28306e31d47a0c4f
zhenyuWang <13641039885@163.com>
2023-08-22 21:23:15 +0800

    update(uts): test project

--- uts/uts-templete/components/Foo.vue
+++ uts/uts-templete/components/Foo.vue
@@ -1,5 +1,6 @@
 <template>
 	<view>
+		<text>data: {{data}}</text>
 		<slot name="header" :msg="msg"></slot>
 		<slot msg="default slot msg"></slot>
 	</view>
@@ -8,11 +9,21 @@
 <script lang="uts">
 	import { SlotsType } from 'vue'
 	export default {
-		name:"Foo",
+		name: "Foo",
 		slots: Object as SlotsType<{
-		  default: { msg: string },
-		  header: { msg: string }
+			default : { msg : string },
+			header : { msg : string }
 		}>,
+		props: {
+			data: {
+				type: String,
+				default: 'default data',
+				validator(value): boolean {
+					console.warn('validator', value)
+					return ['success', 'warning', 'danger'].includes(value)
+				}
+			}
+		},
 		data() {
 			return {
 				msg: 'header slot msg'
