9f875f86b512b417a89cbcb291bc04be95b0ad3a
zhenyuWang <13641039885@163.com>
2023-09-19 22:00:20 +0800

    feat(uts): uts automator simple

---
+++ uts/uts-automator-simple/env.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "compile": true,
+    "h5": {
+        "options": {
+            "headless": true
+        },
+        "executablePath": ""
+    },
+    "mp-weixin": {
+        "port": 9420,
+        "account": "",
+        "args": "",
+        "cwd": "",
+        "launch": true,
+        "teardown": "disconnect",
+        "remote": false,
+        "executablePath": ""
+    },
+    "app-plus": {
+        "android": {
+            "id": "emulator-5554",
+            "executablePath": "/Applications/HBuilderX-Alpha.app/Contents/HBuilderX/plugins/launcher/base/android_base.apk"
+        },
+        "version": "/Applications/HBuilderX-Alpha.app/Contents/HBuilderX/plugins/launcher/base/version.txt",
+        "ios": {
+            "id": "",
+            "executablePath": ""
+        },
+        "uni-app-x": {
+            "version": "/Applications/HBuilderX-Alpha.app/Contents/HBuilderX/plugins/uniappx-launcher/base/version.txt",
+            "android": {
+                "id": "bc75caa3",
+                "executablePath": "/Applications/HBuilderX-Alpha.app/Contents/HBuilderX/plugins/uniappx-launcher/base/android_base.apk"
+            }
+        }
+    }
+}
