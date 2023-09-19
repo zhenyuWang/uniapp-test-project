328e1f98897744cad38c3e07beadf81ca250d864
zhenyuWang <13641039885@163.com>
2023-09-19 21:58:43 +0800

    feat(cli): vue3 pinia

---
+++ cli/vue3-node18-pinia/src/pages/index/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+const counter = useCounterStore()
+counter.count++
+// 自动补全！ ✨
+counter.$patch({ count: counter.count + 1 })
+// 或使用 action 代替
+counter.increment()
+function plus() {
+  counter.increment()
+}
+</script>
+<template>
+  <!-- 直接从 store 中访问 state -->
+  <div @click="plus">Current Count: {{ counter.count }}</div>
+</template>
