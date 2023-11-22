357b48e1ccf6e9352675f1c5db91bd78e4cee754
zhenyuWang <13641039885@163.com>
2023-11-22 21:40:36 +0800

    feat(1.0): v-html

---
+++ uts/1.0-v-html/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view v-html="html"></view>
+	<text class="my-text">test text</text>
+	<rich-text :nodes="strings"></rich-text>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				html: '<text class="my-text" style="color: red;">text for v-html</text>',
+				strings: '<div style="text-align:center;"><div style="color: red;">test text123123123123</div><img src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uni@2x.png"/></div>'
+			}
+    },
+		onReady() {
+			console.log('onReady')
+			uni.getAppBaseInfo()
+			uni.getElementById
+		}
+	}
+</script>
+
+<style>
+	.my-text {
+		font-size: 30px;
+	}
+</style>
