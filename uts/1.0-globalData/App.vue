8fa46a5f832181a59be1bd424d8a69dca3efcb12
zhenyuWang <13641039885@163.com>
2023-11-22 21:40:14 +0800

    feat(1.0): globalData

---
+++ uts/1.0-globalData/App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		globalData: {
+		  str: 'global data str',
+		  num: 123,
+		  bool: true,
+		},
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ uts/1.0-globalData/pages/index/index.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="content">
+		<text>{{globalDataStr}}</text>
+		<text>{{globalDataNum}}</text>
+		<text>{{globalDataBool}}</text>
+		<button @click="setGlobalData">setGlobalData</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				globalDataStr: '',
+				globalDataNum: 0,
+				globalDataBool: false,
+			}
+		},
+		onReady(){
+			this.globalDataStr = getApp().globalData.str
+			this.globalDataNum = getApp().globalData.num
+			this.globalDataBool = getApp().globalData.bool
+		},
+		methods: {
+			setGlobalData() {
+			  const app = getApp()
+			  app.globalData.str = 'new global data str'
+			  app.globalData.num = 456
+			  app.globalData.bool = false
+			},
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
