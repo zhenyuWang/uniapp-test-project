9e8fc3d7aba935e8319ed5f315bf6d6f5f1d21ec
zhenyuWang <13641039885@163.com>
2023-11-29 23:39:11 +0800

    feat: vue3 home page navigateBack reload

---
+++ uts/vue2-home-page-navigateBack-reload/pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<view class="content">
+		<text>{{title}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'about'
+			}
+		}
+	}
+</script>
