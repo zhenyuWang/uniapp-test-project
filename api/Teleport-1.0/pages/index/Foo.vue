3d5b4668cfe7d90eeb87088a7d6d09b89a1e1602
zhenyuWang <13641039885@163.com>
2023-12-29 16:32:07 +0800

    feat(api): Teleport

---
+++ api/Teleport-1.0/pages/index/Foo.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="foo">
+		<text>this is foo component</text>
+		<button @click="changeShow">changeShow</button>
+		<template v-if="show">
+			<Teleport to='#test'>
+				<text>this is node in Teleport from foo to #test</text>
+			</Teleport>
+		</template>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				show: false
+			}
+		},
+		methods: {
+			changeShow() {
+				this.show = !this.show
+			}
+		}
+	}
+</script>
+
+<style>
+	.foo {
+		background-color: green;
+	}
+</style>
---
+++ api/Teleport-1.0/pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="content">
+		<view id="test"></view>
+		<text>this is home page</text>
+		<view>{{title}}</view>
+		<view>{{globalStr}}</view>
+		<view>{{globalNum}}</view>
+		<button @click="test">test</button>
+		<Foo />
+	</view>
+</template>
+
+<script>
+	import Foo from './Foo.vue'
+	export default {
+		components: {
+			Foo
+		},
+		data(){
+			return {
+				title: 'defualt title'
+			}
+		},
+		methods: {
+			test(){
+				this.title = 'new title'
+				this.globalStr = 'new str'
+				this.globalNum = 1234
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		background-color: red;
+	}
+</style>
