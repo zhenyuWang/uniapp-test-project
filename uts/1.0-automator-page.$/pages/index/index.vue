19ae92452562fece6f8bd18f3df08b9b92fd6f94
zhenyuWang <13641039885@163.com>
2024-03-29 21:57:40 +0800

    feat: 1.0 automator page.$

---
+++ uts/1.0-automator-page.$/pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <view class="content">
+    <text class="title">{{title}}</text>
+    <text>count： {{count}}</text>
+  </view>
+</template>
+
+<script setup>
+  // export default {
+  //   data() {
+  //     return {
+  //       title: 'Hello'
+  //     }
+  //   }
+  // }
+  import { ref, watchEffect } from 'vue';
+  
+  const title = 'Hello'
+  
+  const count = ref(0)
+  
+  watchEffect(() => console.log(count.value))
+  // -> 输出 0
+  
+  count.value++
+</script>
