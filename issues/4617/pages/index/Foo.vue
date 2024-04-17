f22b6458c3cc608bc7567d37f04087f2b3b27a1c
zhenyuWang <13641039885@163.com>
2024-04-17 22:16:36 +0800

    feat(issue): 4617

---
+++ issues/4617/pages/index/Foo.vue
@@ -0,0 +1,9 @@
+<template>
+  <view class="foo">
+		<slot :age="48"></slot>
+  </view>
+</template>
+
+<style>
+  .foo .text{}
+</style>
---
+++ issues/4617/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    <Foo class="testA">测试A</Foo>
+    <Foo v-slot="scope">
+      测试B {{scope.age}}
+    </Foo>
+    <Foo>
+      <template #default="scope">
+        测试C {{ scope.age }}
+      </template>
+    </Foo>
+  </view>
+</template>
+
+<script>
+  import Foo from './Foo.vue'
+  export default {
+    components: {
+      Foo
+    },
+  }
+</script>
