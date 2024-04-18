5616d5f3e8a629536a1e5c676f8b5e7a9f3e8fb8
zhenyuWang <13641039885@163.com>
2024-04-18 23:42:53 +0800

    feat(paying user): scanCode

---
+++ paying-user/30802A-scanCode/pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <view class="content">
+    <button @click="test">test</button>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Hello'
+      }
+    },
+    methods: {
+      test() {
+        uni.scanCode({
+          onlyFromCamera: true,
+          success(res) {
+            console.log('scanCode res---------', res)
+          }
+
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
