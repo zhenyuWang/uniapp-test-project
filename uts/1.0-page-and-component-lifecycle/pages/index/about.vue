63a03d1c0b1198e096d894f2467b06e8fda0adc1
zhenyuWang <13641039885@163.com>
2024-04-23 23:26:04 +0800

    feat(1.0): page and component lifecycle

---
+++ uts/1.0-page-and-component-lifecycle/pages/index/about.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="content">
+    <text class="title">{{title}}</text>
+    <Foo />
+  </view>
+</template>
+
+<script>
+  import Foo from './Foo.vue'
+  export default {
+    components: {Foo},
+    data() {
+      return {
+        title: 'this is page title'
+      }
+    },
+    onLoad() {
+      console.log('page onLoad')
+    },
+    onShow() {
+      console.log('page onShow')
+    },
+    onReady() {
+      console.log('page onReady')
+    },
+    onBackPress() {
+      console.log('page onBackPress')
+      return null
+    },
+    onHide() {
+      console.log('page onHide')
+    },
+    onUnload() {
+      console.log('page onUnload')
+    }
+  }
+</script>
---
+++ uts/1.0-page-and-component-lifecycle/pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <view class="content">
+    <text class="title">{{title}}</text>
+    <Foo />
+    <navigator url="/pages/index/about">go about</navigator>
+  </view>
+</template>
+
+<script>
+  import Foo from './Foo.vue'
+  export default {
+    components: {Foo},
+    data() {
+      return {
+        title: 'this is page title'
+      }
+    },
+    onLoad() {
+      console.log('page onLoad')
+    },
+    onShow() {
+      console.log('page onShow')
+    },
+    onReady() {
+      console.log('page onReady')
+    },
+    onBackPress() {
+      console.log('page onBackPress')
+      return null
+    },
+    onHide() {
+      console.log('page onHide')
+    },
+    onUnload() {
+      console.log('page onUnload')
+    },
+  }
+</script>
+
