d1038cb2ab2dde6fb492102cd6db9eb8b9a531d3
zhenyuWang <13641039885@163.com>
2024-04-25 19:48:33 +0800

    feat: 1.0 v-model

---
+++ uts/1.0-v-model/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view>
+    <!-- <text v-text="title"></text>
+    <view v-text="title"></view> -->
+    <text>num: {{num}}</text>
+    <text>typeof num: {{typeof num}}</text>
+    <input v-model.number="num" type="text" />
+    <text>str: {{str}}</text>
+    <text>str.length: {{str.length}}</text>
+    <!-- <input v-model.trim="str" type="text" /> -->
+    <input v-model.lazy="str" type="text" />
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Hello',
+        num: 1,
+        str: 'abc'
+      }
+    }
+  }
+</script>
