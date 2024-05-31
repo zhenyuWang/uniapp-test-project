82b57d1376bab5d1310fe91c16e37f1e4aecabe0
zhenyuWang <13641039885@163.com>
2024-05-31 22:52:51 +0800

    feat: theme change

---
+++ api/themeChange/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+  const fn1 = (options) => {
+    console.log('fn1------', options)
+  }
+  const fn2 = (options) => {
+    console.log('fn2------', options)
+  }
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onReady() {
+      uni.onThemeChange(fn1)
+      uni.onThemeChange(fn1)
+      uni.onThemeChange(fn2)
+		},
+		methods: {
+
+		}
+	}
+</script>
