4734a913850261abd5d010cb706a966ede84f079
zhenyuWang <13641039885@163.com>
2024-06-01 10:41:48 +0800

    update(vue): origin vite vue3

--- cli/origin-vite-vue3/src/App.vue
+++ cli/origin-vite-vue3/src/App.vue
@@ -1,38 +1,7 @@
-<script>
-import Foo from './components/Foo.vue';
-
-export default {
-  name: 'App',
-  components: {
-    Foo,
-  },
-  data() {
-    return {
-      title: 'default page title',
-      uniIcon: '&#xe100;',
-    };
-  },
-  methods: {
-    triggerGlobalMethod() {
-      this.title = 'new title';
-      this.globalPropertiesStr = 'new globalPropertiesStr';
-      this.globalPropertiesNum = 1234;
-    },
-  },
-};
-</script>
-
-<template src='./test.html'>
-  
+<template>
+  <div><Index /></div>
 </template>
 
-<style>
-@font-face {
-  font-family: 'UniFontFamily';
-  src: url('./assets/uni.ttf');
-}
-
-.icon {
-  font-family: 'UniFontFamily';
-}
-</style>
+<script setup>
+  import Index from './index.vue'
+</script>
---
+++ cli/origin-vite-vue3/src/index.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { ref, h } from 'vue'
+const msg = ref('default msg')
+const render = () => (
+  h('div', { class: 'page' }, [
+    h(
+      'div',
+      { class: 'uni-common-mt msg', style: { color: 'blue' } },
+      msg.value
+    ),
+    h(
+      'button',
+      {
+        class: 'uni-common-mt btn',
+        type: 'primary',
+        onClick: () => {
+          msg.value += '1'
+        },
+      },
+      'click'
+    ),
+  ])
+)
+</script>
+
+<template>
+  <render />
+</template>
+<style>
+.btn {
+  color: red;
+}
+</style>
+<!-- 
+<script>
+import { ref, h } from 'vue'
+export default {
+  setup() {
+    const msg = ref('default msg')
+    return () => {
+      return h('div', { class: 'page' }, [
+        h(
+          'div',
+          { class: 'uni-common-mt msg', style: { color: 'blue' } },
+          msg.value
+        ),
+        h(
+          'button',
+          {
+            class: 'uni-common-mt btn',
+            type: 'primary',
+            onClick: () => {
+              msg.value = msg.value + '1'
+            },
+          },
+          'click'
+        ),
+      ])
+    }
+  },
+}
+</script>
+
+<style>
+.btn {
+  color: red;
+}
+</style> -->
