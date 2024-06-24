c1d770c136a0f152502c155e58ebea81b68ca28c
zhenyuWang <13641039885@163.com>
2024-06-24 22:16:46 +0800

    feat(uts): trigger event

---
+++ uts/2.0-automator-trigger-event/env.js
@@ -0,0 +1,42 @@
+module.exports = {
+    "is-custom-runtime": false,
+    "compile": true,
+    "h5": {
+        "options": {
+            "headless": false
+        },
+        "executablePath": ""
+    },
+    "mp-weixin": {
+        "port": 9420,
+        "account": "",
+        "args": "",
+        "cwd": "",
+        "launch": true,
+        "teardown": "disconnect",
+        "remote": false,
+        "executablePath": ""
+    },
+    "app-plus": {
+        "android": {
+            "id": "",
+            "executablePath": ""
+        },
+        "version": "",
+        "ios": {
+            "id": "",
+            "executablePath": ""
+        },
+        "uni-app-x": {
+            "version": "/Applications/HBuilderX-Dev.app/Contents/HBuilderX/plugins/uniappx-launcher/base/version.txt",
+            "android": {
+                "id": "emulator-5554",
+                "executablePath": "/Applications/HBuilderX-Dev.app/Contents/HBuilderX/plugins/uniappx-launcher/base/android_base.apk"
+            },
+            "ios": {
+                "id": "F01B89BE-6BA8-43FA-AC54-442BC6BD4937",
+                "executablePath": "/Applications/HBuilderX-Dev.app/Contents/HBuilderX/plugins/uniappx-launcher/base/Pandora_simulator.app"
+            }
+        }
+    }
+}
