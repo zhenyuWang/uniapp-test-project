673d755d05dd5c67b659f6a169c101b409e9815f
zhenyuWang <13641039885@163.com>
2024-07-20 10:24:01 +0800

    feat(1.0): import json

---
+++ api/1.0-import-json/pages/index/about.vue
@@ -0,0 +1,15 @@
+<template>
+	<view>
+		<text>{{title}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		}
+	}
+</script>
---
+++ api/1.0-import-json/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		<text>{{title}}</text>
+	</view>
+</template>
+
+<script>
+  import pagesJson from '@/pages.json'
+  import {pages} from '@/pages.json'
+	export default {
+		data() {
+			return {
+				title: 'Hello1'
+			}
+		},
+		onLoad() {
+      console.log('pagesJson',pagesJson)
+      console.log('pages',pages)
+		}
+	}
+</script>
