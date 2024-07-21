96354d87228e905807f7b4b8d31666827870611a
zhenyuWang <13641039885@163.com>
2024-07-21 23:25:39 +0800

    feat(uni-app): env

---
+++ api/1.0-env/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <view>
+    <text>{{title}}</text>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Hello vue2'
+      }
+    },
+    onReady(){
+      // const num = import.meta.env.VITE_TEST_NUM
+      // const str = import.meta.env.VITE_TEST_STR
+      const num = process.env.VITE_TEST_NUM
+      const str = process.env.VITE_TEST_STR
+      console.log('num', num)
+      console.log('str', str)
+    }
+  }
+</script>
