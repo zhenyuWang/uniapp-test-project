9be8c0b3e25b4ed7aa8fe76161d04b8e426538ad
zhenyuWang <13641039885@163.com>
2024-08-14 11:25:06 +0800

    feat(2.0): input v-model & value

---
+++ uts/2.0-input-v-model-value/src/App.vue
@@ -0,0 +1,38 @@
+<script lang="uts">
+let firstBackTime = 0
+export default {
+  onLaunch: function (options) {
+    console.log("App Launch ---------------------------------", options);
+  },
+  onShow: function () {
+    console.log("App Show ---------------------------------");
+  },
+  onHide: function () {
+    console.log("App Hide ---------------------------------");
+  },
+  onExit: function () {
+    console.log("App Exit ---------------------------------");
+  },
+  onLastPageBackPress: function () {
+    console.log("App LastPageBackPress ---------------------------------");
+    if (firstBackTime == 0) {
+      console.log('再按一次退出应用---------------')
+      firstBackTime = Date.now()
+      setTimeout(() => {
+        firstBackTime = 0
+      }, 2000)
+    } else if (Date.now() - firstBackTime < 2000) {
+      firstBackTime = Date.now()
+    }
+  }
+};
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
