be4816ee265720eda734672b17efa62861ab4ca9
zhenyuWang <13641039885@163.com>
2024-08-27 16:28:56 +0800

    feat(ask): 153074

---
+++ ask/153074/pages/index/Bar.vue
@@ -0,0 +1,31 @@
+<template>
+  <view :class="myClass" style="padding: 20px 10px;">
+    <text>this is Bar vue</text>
+    <button @click="toggleClass">toggle class</button>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        myClass: 'bg-red-bar'
+      }
+    },
+    methods: {
+      toggleClass() {
+        this.myClass = this.myClass === 'bg-red-bar' ? 'bg-green-bar' : 'bg-red-bar'
+      }
+    }
+  }
+</script>
+
+<style>
+  .bg-red-bar {
+    background-color: red;
+  }
+
+  .bg-green-bar {
+    background-color: green;
+  }
+</style>
