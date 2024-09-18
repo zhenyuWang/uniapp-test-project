7d406a7c36f20c7b0eed96c00f1eb3efa9475fe2
zhenyuWang <13641039885@163.com>
2024-09-18 19:11:13 +0800

    feat(uts): theme change

---
+++ uts/2.0-theme-change/src/App.vue
@@ -0,0 +1,14 @@
+import { onThemeChange } from '@dcloudio/types/uni-app/uni/base/ThemeChange';
+
+<script lang="uts">
+export default {
+  onShow: function (options: OnShowOptions) {
+    console.error("wzf App onShow ---",options);
+  },
+  onError: function (err:any) {
+    console.error('wzf app onError ---')
+    console.error('wzf app onError ---error', err)
+  }
+};
+</script>
+S
