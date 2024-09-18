d44ba48a14c3f921bed1dad2fff8ded187a6f586
zhenyuWang <13641039885@163.com>
2024-09-18 19:10:06 +0800

    feat(uts): page orientation landscape

---
+++ uts/2.0-pageOrientation-landscape/pages/index/index.test.js
@@ -0,0 +1,23 @@
+describe('test title', () => {
+  let page;
+
+  it('index screenShot', async () => {
+    page = await program.reLaunch('/pages/index/index');
+    await page.waitFor(1000);
+    
+    const image = await program.screenshot({
+      fullPage: true
+    });
+    expect(image).toSaveImageSnapshot();
+  })
+  
+  it('about screenShot', async () => {
+    page = await program.reLaunch('/pages/index/about');
+    await page.waitFor(1000);
+    
+    const image = await program.screenshot({
+      fullPage: true
+    });
+    expect(image).toSaveImageSnapshot();
+  })
+});
