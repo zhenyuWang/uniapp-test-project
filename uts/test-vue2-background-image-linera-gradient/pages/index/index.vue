ccb62c20db7fd7d5fd3cb04aabaaddd76a16a8d1
zhenyuWang <13641039885@163.com>
2024-09-22 15:56:38 +0800

    test(1.0 vue2): background image linear gradient

---
+++ uts/test-vue2-background-image-linera-gradient/pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <view>
+    <view class="box1">
+      <text class="title">{{title}}</text>
+      <view class="box2">
+        <text class="title">{{title}}</text>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Hello'
+      }
+    },
+    onLoad() {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+  .box1 {
+    width: 300px;
+    height: 100px;
+    background-image: linear-gradient(to right, yellow, red);
+  }
+
+  .box2 {
+    width: 200px;
+    height: 60px;
+    background-image: linear-gradient(-90deg, rgb(172, 226, 246) 0%, rgb(255, 255, 255) 45%, rgb(255, 255, 255) 100%)
+  }
+</style>
