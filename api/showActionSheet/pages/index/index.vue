7be764c0dd8dee2aa515f42a31f795a70e7e0bdf
zhenyuWang <13641039885@163.com>
2024-10-10 20:39:24 +0800

    feat(1.0-api): showActionSheet

---
+++ api/showActionSheet/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="content">
+		<button @click="showModal">showModal</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+      showModal(){
+        uni.showActionSheet({
+          title:'title test title test title test title test title test title test title test title test title test title test title test title test title test title test title test title test title',
+          alertText: 'my alertText',
+          itemList:['1 test title test title test title test title test title test title test title test title test title test title test title test title test title test title test title test title','2','3'],
+          itemColor:'#0000ff',
+          success: function (res) {
+            console.log('success', res)
+          },
+          fail: function (err) {
+            console.log('fail', err)
+          },
+          complete: function (res) {
+            console.log('complete', res)
+          }
+        })
+      }
+		}
+	}
+</script>
