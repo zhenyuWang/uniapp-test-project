9a6c8e578d9dc108283472b16cbfee2aa306359d
zhenyuWang <13641039885@163.com>
2024-10-10 20:39:36 +0800

    feat(1.0 api): showModal

---
+++ api/showModal/pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="content">
+		<button @click="showModal">showModal</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+      showModal(){
+        uni.showModal({
+          title:'title',
+          content:'content',
+        })
+      }
+		}
+	}
+</script>
