4ca9aa0b8ffb45b2757e8af2120a5d8e7750d752
zhenyuWang <13641039885@163.com>
2024-11-05 15:11:27 +0800

    update: test projects

--- uts/dialogPage/src/App.vue
+++ uts/dialogPage/src/App.vue
@@ -3,8 +3,8 @@ import { onError } from '@dcloudio/types/uni-app/uni/base/Error';
 <script lang="uts">
 export default {
   onLaunch: function (options) {
-    console.log("wzf App Launch ---", options);
-    uni.openDialogPage({url: '/pages/dialog2/index?test=111', animationType: 'fade-in'})
+    console.log("wzf App Launch in uts dialogPage ---", options);
+    // uni.openDialogPage({url: '/pages/dialog2/index?test=111'})
   },
   onShow: function () {
     console.log("wzf App Show ---")
