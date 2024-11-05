54e82af210633049fd3dff709d2eb9460913cf62
zhenyuWang <13641039885@163.com>
2024-11-05 15:06:11 +0800

    update(x): showActionSheet

---
+++ api/x-showActionSheet/env.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "is-custom-runtime": false,
+    "UNI_TEST_CUSTOM_ENV": {},
+    "compile": true,
+    "h5": {
+        "options": {
+            "headless": false
+        },
+        "executablePath": ""
+    },
+    "mp-weixin": {
+        "port": 9420,
+        "account": "",
+        "args": "",
+        "cwd": "",
+        "launch": true,
+        "teardown": "disconnect",
+        "remote": false,
+        "executablePath": ""
+    },
+    "app-plus": {
+        "android": {
+            "id": "",
+            "executablePath": ""
+        },
+        "version": "",
+        "ios": {
+            "id": "",
+            "executablePath": ""
+        }
+    }
+}
