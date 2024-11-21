6124b8acac4673c87b625df1fc7dd8c1aebe3684
zhenyuWang <13641039885@163.com>
2024-11-21 20:26:15 +0800

    feat(weixin): chooseLocation

---
+++ api/1.0-test-chooseLocation/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <view>
+    <button @click="test">chooseLocation</button>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Hello'
+      }
+    },
+    onLoad() {
+
+    },
+    methods: {
+      test() {
+        uni.chooseLocation({
+          success: (res) => {
+            console.log('chooseLocationRes: ', res);
+          },
+          fail: (err) => {
+            console.log('err: ', err)
+          },
+          complete: (res) => {
+            console.log('complete: ', res)
+          }
+        })
+      }
+    }
+  }
+</script>
---
+++ api/1.0-weixin-chooseLocation/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<text>this is index page</text>
+    <button @click="test">test</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+    onShow(){
+      console.log('index page show');
+    },
+    onHide(){
+      console.log('index page onHide');
+    },
+		methods: {
+      test(){
+        uni.chooseLocation({
+          complete(res){
+            console.log('uni.chooseLocation complete', res);
+          }
+        })
+      }
+		}
+	}
+</script>
