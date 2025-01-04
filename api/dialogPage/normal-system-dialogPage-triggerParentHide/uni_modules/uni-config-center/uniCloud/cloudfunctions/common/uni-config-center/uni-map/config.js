09050c0252dabac143152b986243fb5f6b893da4
zhenyuWang <13641039885@163.com>
2025-01-04 17:25:04 +0800

    feat(uni-app-x): normal and system dialogPage trigger parent hide

---
+++ api/dialogPage/normal-system-dialogPage-triggerParentHide/uni_modules/uni-config-center/uniCloud/cloudfunctions/common/uni-config-center/uni-map/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	"default": "qqmap", // 默认使用的平台
+	"key": {
+		//"qqmap": "L5FBZ-IVCCB-22FUM-J7PKP-TZHF7-4SBAT", // 腾讯地图key
+    "qqmap": "DL6BZ-WYWY4-WRSU7-DQCAA-453XV-HWFOR", // 腾讯地图key
+		"amap": "f6e013da27f4a0dc846a2d33a10b104c", // 高德地图key
+	}
+}
